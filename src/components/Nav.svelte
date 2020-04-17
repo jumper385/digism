8c791e792fd30125b99ef578483bc5f7f25ccb1e
chenry <henrychen385@gmail.com>
2020-04-17 23:33:47 +0800

    Under Construction

---
+++ src/components/Nav.svelte
@@ -0,0 +1,77 @@
+<script>
+	export let segment;
+</script>
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Nunito+Sans&family=Playfair+Display:wght@400;800&display=swap');
+
+	nav {
+		box-sizing: border-box;
+		display:flex;
+		align-items:center;
+		padding:0 32pt;
+		max-width:500pt;
+		margin:0 auto;
+		justify-content: space-between
+	}
+
+	nav .holder {
+		height:100%;
+		margin:0;
+		display:flex;
+		align-items: center;
+		margin-bottom: 8pt;
+	}
+
+	nav .sitetitle {
+		font-family: 'Playfair Display', serif;
+		font-size:1.2em;
+		margin:0;
+	}
+
+	nav .linkholder {
+		text-decoration: none;
+		max-width: 200pt;
+		width:100%;
+		text-align: right;
+	}
+
+	nav .navlink {
+		text-transform: capitalize;
+		text-decoration: none;
+		padding-bottom:8pt;
+		padding: 0 6pt;
+	}
+
+	@media (max-width: 500pt) {
+		nav {
+			padding-top: 24pt;
+			box-sizing: border-box;
+			flex-direction: column;
+		}
+
+		nav .linkholder {
+			width:100%;
+			display: grid;
+			grid-template-columns: 1fr 1fr 1fr;
+			justify-items: center;
+		}
+
+		nav .navlink {}
+	}
+
+</style>
+
+<nav>
+
+	<div class='holder'>
+		<img src='https://media.giphy.com/media/LPkczVwUYcMbXsRCdP/giphy.gif' style='height:36pt' alt='cool emoji'/>
+		<p class='sitetitle'><span style='font-weight:800'>Digi</span><i>sm</i></p>
+	</div>
+
+	<div class='linkholder'>
+		<a class='navlink' aria-current='{segment === undefined ? "page" : undefined}' href='.'>home</a>
+		<a class='navlink disabled' aria-current='{segment === "about" ? "page" : undefined}' href='about'>about</a>
+		<a class='navlink disabled' rel=prefetch aria-current='{segment === "contact" ? "page" : undefined}' href='contact'>contact</a>
+	</div>
+</nav>
---
+++ src/routes/index.svelte
@@ -0,0 +1,27 @@
+<style>
+
+	.landing {
+		width:100%;
+		display:flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.landing-gif {
+		margin:0 auto;
+		width:100%;
+		max-width: 400px;
+	}
+
+</style>
+
+<svelte:head>
+	<title>Digism</title>
+</svelte:head>
+
+<div class='landing'>
+	<h1 style='text-align:center'>Site Under Construction</h1>
+	<img class='landing-gif' src='https://media.giphy.com/media/hp3dmEypS0FaoyzWLR/giphy.gif'/>
+	<p>🥺 Trust me! It's gonna 🔥</p>
+</div>
