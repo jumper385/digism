9ab2c702663379dca6b6e606f2ef61521f98cae1
Henry Chen <henrychen385@gmail.com>
2020-04-18 14:08:39 +0800

    removed ellipsis

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -21,7 +21,7 @@
 </svelte:head>
 
 <div class='landing'>
-	<h1 style='text-align:center'>Site under Construction...</h1>
+	<h1 style='text-align:center'>Site Under Construction<h1>
 	<img class='landing-gif' src='https://media.giphy.com/media/hp3dmEypS0FaoyzWLR/giphy.gif'  alt="I'm working hard... trust me."/>
 	<p>🥺 Trust me! It's gonna be 🔥</p>
 </div>
